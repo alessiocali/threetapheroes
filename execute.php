4b0e43bbe253b1ef92ad5ffdfd0d6ea1074752c4
Alessio Calì <Alessio Cal>
2016-10-06 19:07:55 +0200

    Added Heroku PHP scripts

---
+++ execute.php
@@ -0,0 +1,25 @@
+<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if(!$update)
+{
+  exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+$text = trim($text);
+$text = strtolower($text);
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
+$BOT_TOKEN = '{TOKEN}';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
