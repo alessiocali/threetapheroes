1cdeb0fde9667176f8a51419ffdc3993c3c7dccc
JOUET Romain <contact@jouetromain.fr>
2016-10-07 23:42:09 +0200

    WEBHOOK must be an array not a string
    see https://telegram-bot-sdk.readme.io/docs/setwebhook

--- _token.php
+++ _token.php
@@ -2,5 +2,5 @@
     //Put your bot related informations here
     $APP_NAME = "Your App Name Here";
     $BOT_TOKEN = "1234:MyAwesomeBot";
-    $WEBHOOK = "http://1234.superawesomehost.com";
+    $WEBHOOK = ['url' => 'https://example.com/<token>/webhook']; //https://telegram-bot-sdk.readme.io/docs/setwebhook
 ?>
