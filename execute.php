3612ab4f22ecdc483f89bcbeafc051765cdd506f
Alessio Calì <Alessio Cal>
2016-10-07 21:09:32 +0200

    Fixed a flaw in getWebhookUpdates

--- execute.php
+++ execute.php
@@ -5,11 +5,9 @@ include("_token.php");
 
 $telegram = new Api($BOT_TOKEN);
 
-$updates = $telegram->getWebhookUpdates();
+$update = $telegram->getWebhookUpdates();
 
 $msg = "Hi! This bot is currently a Work in Progress. Please stay tuned!";
 
-foreach ($updates as $update) {
-    $chat_id = $update->getMessage()->getChat()->getId();
-    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $msg]);
-}
+$chat_id = $update->getMessage()->getChat()->getId();
+$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $msg]);
