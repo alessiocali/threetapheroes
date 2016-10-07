485c6551dd282c7660da1ff8f66bdf94640bd0df
Alessio Calì <Alessio Cal>
2016-10-07 21:28:12 +0200

    Actual deployment of BOT, updated dependencies to development versions

--- execute.php
+++ execute.php
@@ -6,8 +6,13 @@ include("_token.php");
 $telegram = new Api($BOT_TOKEN);
 
 $update = $telegram->getWebhookUpdates();
+$callback_id = $update->getCallbackQuery() ? $update->getCallbackQuery()->getId() : "";
 
-$msg = "Hi! This bot is currently a Work in Progress. Please stay tuned!";
+if (!$callback_id) {
+    $msg = "Hi! This bot is currently a Work in Progress. Please stay tuned!";
 
-$chat_id = $update->getMessage()->getChat()->getId();
-$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $msg]);
+    $chat_id = $update->getMessage()->getChat()->getId();
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $msg]);    
+} else {
+    $telegram->answerCallbackQuery(['callback_query_id' => $callback_id, 'url' = "https://threetapheroes.herokuapp.com/"]);
+}
