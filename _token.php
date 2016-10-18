f2f19b8150dee19e21b6571d651c966e54f27120
Alessio Calì <Alessio Cal>
2016-10-19 00:04:16 +0200

    Token issues amended

---
+++ _token.php
@@ -0,0 +1,6 @@
+<?php
+    //Put your bot related informations here
+    $APP_NAME = "MyPrettyBot";
+    $BOT_TOKEN = "MyAwesomeToken";
+    $WEBHOOK = ['url' => 'https://myawesomeurl.whatever']; //https://telegram-bot-sdk.readme.io/docs/setwebhook
+?>
