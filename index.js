1bd397ae9776baa6d2a04a8dd2df0d075070e277
DESKTOP-I33ALP2\alexa <alexander92@hotmail.it>
2017-07-31 14:37:35 +0200

    Moved bot inside index.js. Added WebHook processing

--- index.js
+++ index.js
@@ -3,11 +3,64 @@ var TelegramBot = require('node-telegram-bot-api');
 var express = require('express');
 var path = require('path');
 
-var bot = new TelegramBot(tokens.BOT_TOKEN, {polling:true});
+var bot = new TelegramBot(tokens.BOT_TOKEN, {polling:true, webhook: { port: tokens.PORT || tokens.DEFAULT_PORT }});
 var app = express();
 
 app.set('port', tokens.PORT || tokens.DEFAULT_PORT);
 app.use(express.static(path.join(__dirname + '/html')))
+
+bot.setWebHook(tokens.WEBHOOK+"/bot"+tokens.BOT_TOKEN);
+ 
+bot.on('callbackQuery', (query) => {
+    var url =   tokens.GAME_URL + "?uid=" + query.from.id + 
+                "&iid=" + query.inline_message_id;
+    bot.answerCallbackQuery({
+        'callback_query_id' : query.id,
+        'url' : url
+    });
+});
+
+bot.on('message', (msg) => {
+    var text = msg.text;
+    var chatId = msg.chat.id;
+    
+    if (text === "/start" || text === "/help") {
+        bot.sendMessage({
+            'chat_id' : chatId,
+            'text' :    "Hi! This is the bot for Three Tap Heroes.\n" +
+                        "Commands:\n" +
+                        "- /help Shows this message\n" +
+                        "- /instructions Prints the instructions for the game\n" +
+                        "- /credits Shows the credits\n" +
+                        "- /game Sends the game"
+        });
+    }
+    else if (text === "/instructions") {
+        bot.sendMessage({
+            'chat_id' : chatId,
+            'text' :    "Three young heroes are standing between an army of monsters and " +
+                        "the innocent villagers! Help them coordinating their attacks to repel " +
+                        "the enemies. But beware! Only the frontmost enemy can be damaged. " +
+                        "If you miss and hit another target, you will loose hearts! If you run " +
+                        "out of hearts or the enemy reaches the heroes, it's Game Over!"
+        });
+    }
+    else if (text === "/credits") {
+        fs.readFile('../CREDITS.md', (err, data) => {
+            var answer = err ? "Somebody stole the CREDITS! Please wait while we call the Web Police" : data;
+            bot.sendMessage({
+                'chat_id' : chatId,
+                'text' : answer
+            });
+        });
+    }
+    else if (text === "/game") {
+        bot.sendGame({
+            'chat_id' : chatId,
+            'game_short_name' : tokens.GAME_NAME
+        });
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html');
@@ -29,6 +82,11 @@ app.get('/setscore/uid/:user_id/iid/:inline_id', (req, res) => {
         'inline_message_id' : req.inline_id,
         'edit_message' : true
     });
+});
+
+app.post("/bot"+tokens.BOT_TOKEN, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(app.get('port'), () => {
